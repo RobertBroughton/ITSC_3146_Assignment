fe33a652e11cde79f11ae63c402e719f5620b023
Robert <bobsaidhi01@gmail.com>
2024-09-25 22:09:45 -0400

    Implemented Cashier Functions

--- .venv/cashier.py
+++ .venv/cashier.py
@@ -3,11 +3,14 @@ class Cashier:
         pass
 
     def process_coins(self):
-        """Returns the total calculated from coins inserted.
-           Hint: include input() function here, e.g. input("how many quarters?: ")"""
-        ###
+        """Returns the total calculated from coins inserted."""
+        total = 0
+        total += input("how many large dollars?: ")
+        total += (input("how many half dollars?: ") * .5)
+        total += (input("how many quarters?: ") * .25)
+        total += (input("how many nickles?: ") * .05)
+        return total
 
     def transaction_result(self, coins, cost):
-        """Return True when the payment is accepted, or False if money is insufficient.
-           Hint: use the output of process_coins() function for cost input"""
-        ##
+        """Return True when the payment is accepted, or False if money is insufficient."""
+        return coins >= cost
