a874e4a2dfa7dc2388591fa9c1ce359fd817027f
Robert <bobsaidhi01@gmail.com>
2024-09-25 22:10:05 -0400

    Implemented sandwich_maker Functions

--- .venv/sandwich_maker.py
+++ .venv/sandwich_maker.py
@@ -4,7 +4,9 @@ class SandwichMaker:
 
     def check_resources(self, ingredients):
         """Returns True when order can be made, False if ingredients are insufficient."""
-        #####
+        return self.bread >= ingredients["bread"] & self.ham >= ingredients["ham"] & self.cheese >= ingredients["cheese"]
 
     def make_sandwich(self, sandwich_size, order_ingredients):
-        ########
+        self.bread -= order_ingredients[sandwich_size]["bread"]
+        self.ham -= order_ingredients[sandwich_size]["ham"]
+        self.cheese -= order_ingredients[sandwich_size]["cheese"]
